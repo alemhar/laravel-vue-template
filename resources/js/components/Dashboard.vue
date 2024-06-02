bfcf889673c4d82a99b7697131c6d2f10c0425d1
Alemhar <alemhar@gmail.com>
2024-06-02 20:25:18 +0800

    Remove appointments card

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -4,16 +4,7 @@ import { onMounted, ref } from 'vue';
 const selectedAppointmentStatus = ref('all');
 const totalAppointmentsCount = ref(0);
 
-const getAppointmentsCount = () => {
-    axios.get('/api/stats/appointments', {
-        params: {
-            status: selectedAppointmentStatus.value,
-        }
-    })
-    .then((response) => {
-        totalAppointmentsCount.value = response.data.totalAppointmentsCount;
-    })
-};
+
 
 const selectedDateRange = ref('today');
 const totalUsersCount = ref(0);
@@ -30,7 +21,6 @@ const getUsersCount = () => {
 };
 
 onMounted(() => {
-    getAppointmentsCount();
     getUsersCount();
 });
 </script>
@@ -54,29 +44,7 @@ onMounted(() => {
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-3 col-6">
-                    <div class="small-box bg-info">
-                        <div class="inner">
-                            <div class="d-flex justify-content-between">
-                                <h3>{{ totalAppointmentsCount }}</h3>
-                                <select v-model="selectedAppointmentStatus" @change="getAppointmentsCount()" style="height: 2rem; outline: 2px solid transparent;" class="px-1 rounded border-0">
-                                    <option value="all">All</option>
-                                    <option value="scheduled">Scheduled</option>
-                                    <option value="confirmed">Confirmed</option>
-                                    <option value="cancelled">Cancelled</option>
-                                </select>
-                            </div>
-                            <p>Appointments</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-bag"></i>
-                        </div>
-                        <router-link to="/admin/appointments" class="small-box-footer">
-                            View Appointments
-                            <i class="fas fa-arrow-circle-right"></i>
-                        </router-link>
-                    </div>
-                </div>
+                
 
                 <div class="col-lg-3 col-6">
                     <div class="small-box bg-info">
