483e739637a1e1a342e4aeadbf4102bef4ffcfe2
Alemhar <alemhar@gmail.com>
2024-06-02 20:26:42 +0800

    Persist auth user.

--- resources/js/stores/AuthUserStore.js
+++ resources/js/stores/AuthUserStore.js
@@ -10,16 +10,25 @@ export const useAuthUserStore = defineStore('AuthUserStore', () => {
         avatar: '',
     });
 
+    // Load user data from localStorage during store initialization
+    const savedUser = JSON.parse(localStorage.getItem('authUser'));
+    if (savedUser) {
+        user.value = savedUser;
+    }
+
     const getAuthUser = async () => {
         await axios.get('/api/profile')
             .then((response) => {
                 user.value = response.data;
+                localStorage.setItem('authUser', JSON.stringify(response.data));
             });
+
     };
 
     const setAuthUser = (data) => {
         console.log('setAuthUser', data);
         user.value = data;
+        localStorage.setItem('authUser', JSON.stringify(data));
     };
 
     return { user, getAuthUser, setAuthUser };
